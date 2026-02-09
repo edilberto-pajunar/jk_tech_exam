0929dacf56b4e97bd5bdaa71758755daa12a64c7
edilberto-pajunar <espajunarjr@gmail.com>
2026-02-09 19:00:31 +0800

    implement splash and launcher icons

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -3,5 +3,8 @@ import 'package:jk_tech_exam/features/dashboard/community/data/model/user.dart';
 abstract class AuthRepository {
   Future<AppUser> validateEmail(String email);
   Future<AppUser> login({required String email, required String password});
-  Future<AppUser> createAccount({required String password, required String email});
+  Future<AppUser> createAccount({
+    required String password,
+    required String email,
+  });
 }
